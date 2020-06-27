647d8ffed2502830263e77dd7e9704efe735aa56
rikumiyu <rikumiyu@tencent.com>
2020-06-27 22:53:52 +0800

    feat: v1.0

---
+++ README.md
@@ -0,0 +1,54 @@
+# Icon Sur: macOS Big Sur Adaptive Icon Generator
+
+`iconsur` is a command line tool to easily generate macOS Big Sur styled adaptive icons for third-party apps.
+
+The generation is based on the most related iOS App from the App Store, or, if there isn't one, is created from the original icon, in which case the background color and the scaling can be customized.
+
+![image](https://user-images.githubusercontent.com/5051300/85924963-3b889c00-b8c8-11ea-957a-bf10f5efa068.png)
+
+## Usage
+
+You need to have latest Node.js installed:
+
+```sh
+brew install nodejs
+```
+
+Install `iconsur` from NPM:
+
+```sh
+npm i -g iconsur
+```
+
+Start generating your first adaptive App icon:
+
+```sh
+sudo iconsur set /Applications/Microsoft\ Word.app/
+
+# Update the system icon cache and reload Finder & Dock
+sudo iconsur cache
+
+# Then the new icon will appear, and will last until the App is updated next time.
+```
+
+This will search for the App Store and use the most related iOS App. For Apps from the Mac App Store, `sudo` is required to set the alternative icon.
+
+By default, the name for the macOS App is used to search for a corresponding iOS App. You can change the keyword by specifying `-n`/`--name`.
+
+For Apps that does not have a corresponding iOS App, an irrelevant App may be found. In these cases, you may need to specify the `-a`/`--alternative` option to forcibly generate an adaptive icon:
+
+```sh
+sudo iconsur set /Applications/Visual\ Studio\ Code.app/ -a
+sudo iconsur cache
+```
+
+By default, the original App icon is scaled by 0.9 and is applied to a white background. You may like to change the scaling and background color of the adaptive icon:
+
+```sh
+sudo iconsur set /Applications/Visual\ Studio\ Code.app/ -a -s 0.8 -c 87cdf0
+sudo iconsur cache
+```
+
+## Known Issues
+
+- During generation of adaptive icons, some original `.icns` file may be created in bad formats which will raise an error, such as `No icon was found in file AppIcon.icns` or `Unsupported MIME type: image/jp2`.
---
+++ src/index.js
@@ -0,0 +1,159 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+const cp = require('child_process');
+
+const glob = require('glob');
+const jimp = require('jimp');
+const plist = require('plist');
+const icns = require('icns-lib');
+const cheerio = require('cheerio');
+const { program } = require('commander');
+const { default: fetch } = require('node-fetch');
+
+const { version } = require('../package.json');
+
+process.on('unhandledRejection', (e) => { throw e });
+process.on('uncaughtException', (e) => {
+  console.error('Error:', e.message);
+  process.exit(1);
+});
+
+program.name('iconsur');
+program.version(version);
+program.option('-a, --adaptive', 'Directly create adaptive icon without searching for an iOS App');
+program.option('-k, --keyword <keyword>', 'Specify custom keyword to search for an iOS App');
+program.option('-s, --scale <float>', 'Specify scale for adaptive icon (default: 0.9)');
+program.option('-c, --color <hex>', 'Specify color for adaptive icon (default: ffffff)');
+
+program.command('set <dir> [otherDirs...]').action(async (dir, otherDirs) => {
+  if (!otherDirs.length && ~dir.indexOf('*')) {
+    [dir, ...otherDirs] = glob.sync(dir);
+  }
+
+  for (let appDir of [dir, ...otherDirs]) {
+    console.log(`Processing ${appDir}...`);
+
+    appDir = path.resolve(process.cwd(), appDir);
+    if (!fs.statSync(appDir).isDirectory()) {
+      console.error(`${appDir}: No such directory`);
+      process.exit(1);
+    }
+    
+    if (!appDir.endsWith('.app')) {
+      console.error(`${appDir}: Not an App directory`);
+      process.exit(1);
+    }
+    
+    let appName = program.keyword;
+    let srcIconFile;
+    
+    try {
+      const infoPlist = path.join(appDir, 'Contents/Info.plist');
+      const infoPlistContents = plist.parse(fs.readFileSync(infoPlist, 'utf-8'));
+      if (!appName) {
+        appName = infoPlistContents['CFBundleDisplayName'] || path.basename(appDir).replace(/\.app$/, '');
+      }
+      srcIconFile = path.resolve(appDir, 'Contents/Resources', infoPlistContents['CFBundleIconFile']);
+      if (!/\.icns$/.test(srcIconFile)) {
+        srcIconFile += '.icns';
+      }
+    } catch (e) {
+      if (!program.keyword) {
+        console.log(`Plist file might be corrupted; using fallback name and AppIcon.icns as default icon location.`);
+        console.log('Re-run with option -n or --name to specify custom app name to search for.');
+      } else {
+        console.log(`Plist file might be corrupted; using AppIcon.icns as default icon location.`);
+      }
+      appName = path.basename(appDir).replace(/\.app$/, '');
+      srcIconFile = path.resolve(appDir, 'Contents/Resources/AppIcon.icns');
+    }
+    
+    const imageSize = 256;
+    const iconPadding = Math.floor(imageSize * 0.1);
+    const iconSize = imageSize - 2 * iconPadding;
+    const originalIconScaleSize = parseFloat(program.scale || '0.9');
+    const mask = (await jimp.read(path.resolve(__dirname, 'mask.png'))).resize(iconSize, iconSize);
+    let resultIcon;
+    let iOSApp = null;
+  
+    if (!program.adaptive) {
+      console.log(`Searching iOS App with name: ${appName}`);
+      const res = await fetch(`https://www.apple.com/search/${encodeURIComponent(appName)}?page=1&sel=explore&src=globalnav`);
+      const $ = cheerio.load(await res.text());
+      iOSApp = $('.as-explore-product');
+    }
+  
+    if (iOSApp && iOSApp.length) {
+      const appName = iOSApp.find('.as-productname').eq(0).text();
+      const iconUrl = iOSApp.find('.as-explore-img').eq(0).attr('src');
+      console.log(`Found iOS app: ${appName} with icon: ${iconUrl}`);
+      console.log(`If this app is incorrect, specify the correct name with -k or --keyword, or generate an adaptive icon with option -a or --adaptive`);
+      const res = await fetch(iconUrl);
+      const iconData = await res.buffer();
+      const unmaskedImage = (await jimp.read(iconData)).resize(iconSize, iconSize);
+      resultIcon = unmaskedImage.mask(mask, 0, 0);
+    } else {
+      if (!program.adaptive) {
+        console.log(`Cannot find iOS App with name: ${appName}`);
+      }
+
+      console.log(`Generating adaptive icon...`);
+      if (!fs.existsSync(srcIconFile)) {
+        console.error(`Cannot find icon at ${srcIconFile}`);
+        process.exit(1);
+      }
+      
+      const scalePosition = iconSize * (1 - originalIconScaleSize) / 2;
+      const iconBuffer = Object.entries(icns.parse(fs.readFileSync(srcIconFile)))
+        .filter(([k]) => icns.isImageType(k))
+        .map(([, v]) => v)
+        .sort((a, b) => b.length - a.length)[0];
+      
+      if (!iconBuffer) {
+        console.error(`No icon was found in file ${srcIconFile}`);
+        process.exit(1);
+      }
+      const originalIcon = (await jimp.read(iconBuffer)).resize(iconSize * originalIconScaleSize, iconSize * originalIconScaleSize);
+      resultIcon = (await jimp.create(iconSize, iconSize, program.color || '#ffffff')).mask(mask, 0, 0).composite(originalIcon, scalePosition, scalePosition);
+    }
+  
+    const image = (await jimp.create(imageSize, imageSize, 0)).composite(resultIcon, iconPadding, iconPadding);
+    const tmpFile = path.resolve(os.tmpdir(), `tmp-${Math.random().toFixed(16).substr(2, 6)}.png`);
+    await image.writeAsync(tmpFile);
+  
+    const { status } = cp.spawnSync(path.join(__dirname, 'fileicon.sh'), ['set', appDir, tmpFile], { stdio: 'inherit' });
+    if (status) {
+      console.error(`Failed to set custom icon: fileicon script exited with error ${status}`);
+      process.exit(1);
+    }
+
+    console.log(`Successfully set icon for ${appDir}\n`);
+  };
+});
+
+program.command('unset <dir> [otherDirs...]').action(async (dir, otherDirs) => {
+  if (!otherDirs.length && ~dir.indexOf('*')) {
+    [dir, ...otherDirs] = glob.sync(dir);
+  }
+
+  for (let appDir of [dir, ...otherDirs]) {
+    const { status } = cp.spawnSync(path.join(__dirname, 'fileicon.sh'), ['rm', appDir], { stdio: 'inherit' });
+    if (status) {
+      console.error(`Failed to remove custom icon: fileicon script exited with error ${status}`);
+      process.exit(1);
+    }
+  }
+});
+
+program.command('cache').action(() => {
+  cp.execSync('sudo rm -rf /Library/Caches/com.apple.iconservices.store', { stdio: 'inherit' });
+  cp.execSync('sudo find /private/var/folders/ \\( -name com.apple.dock.iconcache -or -name com.apple.iconservices \\) -exec rm -rfv {} \\;', { stdio: 'inherit' });
+  cp.execSync('sleep 3; sudo touch /Applications/*', { stdio: 'inherit' });
+  cp.execSync('killall Dock', { stdio: 'inherit' });
+  cp.execSync('killall Finder', { stdio: 'inherit' });
+  process.exit();
+});
+
+program.parse(process.argv);
